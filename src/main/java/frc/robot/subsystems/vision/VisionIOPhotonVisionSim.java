3fc90c1493ee7a12241fc02f51ae8c6644876cc4
Dean Davis <dean.davis@safholland.com>
2025-01-08 14:41:33 -0500

    Vision stuff

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
@@ -16,6 +16,7 @@ package frc.robot.subsystems.vision;
 import static frc.robot.subsystems.vision.VisionConstants.aprilTagLayout;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import java.util.function.Supplier;
 import org.photonvision.simulation.PhotonCameraSim;
@@ -48,6 +49,7 @@ public class VisionIOPhotonVisionSim extends VisionIOPhotonVision {
 
     // Add sim camera
     var cameraProperties = new SimCameraProperties();
+    cameraProperties.setCalibration(640, 480, Rotation2d.fromDegrees(70));
     cameraSim = new PhotonCameraSim(camera, cameraProperties);
     visionSim.addCamera(cameraSim, robotToCamera);
   }
