8ec48d73bbcf4673131a4d4ca6940c385e734f7b
Team 3572 <team3572@gmail.com>
2025-01-14 17:52:25 -0500

    More Elevator Code

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems.elevator;
 
+import edu.wpi.first.math.util.Units;
+
 public class ElevatorConstants {
   public static final int ElevatorCanId = 99;
-  public static final double motorReduction = 1.0;
+  public static final double motorReduction = 11.0 / 1630; // Rotations / Max Height in mm
   public static final int currentLimit = 40;
+  public static final double kElevatorGearing = 10.0;
+  public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0);
+  public static final double kCarriageMass = 4.0; // kg
 }
