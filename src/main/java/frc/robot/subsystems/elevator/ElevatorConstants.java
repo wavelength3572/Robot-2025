dc91d59e44964e6861d3b7d34f1fbf18e35d99ce
Dean Davis <dean.davis@safholland.com>
2025-01-17 15:07:53 -0500

    Elevator Sim Stuff

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -6,7 +6,12 @@ public class ElevatorConstants {
   public static final int ElevatorCanId = 99;
   public static final double motorReduction = 11.0 / 1630; // Rotations / Max Height in mm
   public static final int currentLimit = 40;
-  public static final double kElevatorGearing = 10.0;
-  public static final double kElevatorDrumRadius = Units.inchesToMeters(2.0);
+  public static final double kElevatorGearing = 25.0;
+  public static final double kElevatorDrumRadius = Units.inchesToMeters(1.3) / 2.0;
   public static final double kCarriageMass = 4.0; // kg
+
+  public static final double kPixelsPerMeter = 20;
+
+  public static final double kMinElevatorHeightMeters = 0.922; // m
+  public static final double kMaxElevatorHeightMeters = 1.62; // m
 }
