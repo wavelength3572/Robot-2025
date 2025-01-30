0333d1f25fa54cf84c45914eff7fcd36fe91f61a
Team 3572 <team3572@gmail.com>
2025-01-30 18:14:57 -0500

    Elevator Tuning

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,11 +1,23 @@
 package frc.robot.subsystems.elevator;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class Elevator extends SubsystemBase {
   private final ElevatorIO io;
   private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+
+  private static final LoggedTunableNumber ElevatorkP =
+      new LoggedTunableNumber("Elevator/kEp", ElevatorConstants.kElevatorKp);
+  private static final LoggedTunableNumber ElevatorkD =
+      new LoggedTunableNumber("Elevator/kEd", ElevatorConstants.kElevatorKd);
+  private static final LoggedTunableNumber ElevatorkF =
+      new LoggedTunableNumber("Elevator/kEf", ElevatorConstants.kElevatorKf);
+  private static final LoggedTunableNumber ElevatorVel =
+      new LoggedTunableNumber("Elevator/kEVel", ElevatorConstants.kElevatorVel);
+  private static final LoggedTunableNumber ElevatorAcc =
+      new LoggedTunableNumber("Elevator/kEAcc", ElevatorConstants.kElevatorAcc);
 
   public Elevator(ElevatorIO io) {
     this.io = io;
@@ -13,6 +25,18 @@ public class Elevator extends SubsystemBase {
 
   @Override
   public void periodic() {
+    if (ElevatorkP.hasChanged(hashCode())
+        || ElevatorkD.hasChanged(hashCode())
+        || ElevatorkF.hasChanged(hashCode())
+        || ElevatorVel.hasChanged(hashCode())
+        || ElevatorAcc.hasChanged(hashCode())) {
+      io.setPIDValues(
+          ElevatorkP.get(),
+          ElevatorkD.get(),
+          ElevatorkF.get(),
+          ElevatorVel.get(),
+          ElevatorAcc.get());
+    }
     io.updateInputs(inputs);
     Logger.processInputs("Elevator", inputs);
   }
