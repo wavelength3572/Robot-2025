26ab19fc37ff48392ab96bde0e1c793199b1a5ae
Team 3572 <team3572@gmail.com>
2025-02-03 19:20:16 -0500

    Bad Coral Preset files

---
+++ src/main/java/frc/robot/subsystems/coral/CoralSystemPresetChooser.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.coral;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class CoralSystemPresetChooser {
+  private final SendableChooser<CoralSystemPresets> chooser = new SendableChooser<>();
+
+  public CoralSystemPresetChooser() {
+    // Set a default option
+    chooser.setDefaultOption("Stow", CoralSystemPresets.STOW);
+
+    chooser.addOption("Stow Low", CoralSystemPresets.STOW_LOW);
+
+    // Add all your enum options
+    chooser.addOption("Pickup", CoralSystemPresets.PICKUP);
+    chooser.addOption("Score Level 1", CoralSystemPresets.SCORE_LEVEL_1);
+    chooser.addOption("Score Level 2", CoralSystemPresets.SCORE_LEVEL_2);
+    chooser.addOption("Score Level 3", CoralSystemPresets.SCORE_LEVEL_3);
+    chooser.addOption("Score Level 4", CoralSystemPresets.SCORE_LEVEL_4);
+    chooser.addOption(
+        "Front Algae Dislodge Level 1", CoralSystemPresets.FRONT_ALGAE_DISLODGE_LEVEL_1);
+    chooser.addOption(
+        "Front Algae Dislodge Level 2", CoralSystemPresets.FRONT_ALGAE_DISLODGE_LEVEL_2);
+    chooser.addOption(
+        "Back Algae Dislodge Level 1", CoralSystemPresets.BACK_ALGAE_DISLODGE_LEVEL_1);
+    chooser.addOption(
+        "Back Algae Dislodge Level 2", CoralSystemPresets.BACK_ALGAE_DISLODGE_LEVEL_2);
+
+    // Publish the chooser to the dashboard
+    SmartDashboard.putData("Coral Position Chooser", chooser);
+  }
+
+  /** Returns the currently selected CoralPosition. */
+  public CoralSystemPresets getSelected() {
+    return chooser.getSelected();
+  }
+}
